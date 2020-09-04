4aec4a4269d6edebe0f67f825a446be64dae6bf2
David <davidwisth@gmail.com>
2020-09-04 16:20:30 +0100

    Fix undistorted timestamp output for velodyne

--- lidar_undistortion/src/velodyne_undistorter_ros.cpp
+++ lidar_undistortion/src/velodyne_undistorter_ros.cpp
@@ -95,7 +95,8 @@ void VelodyneUndistorterROS::scanCallback(const velodyne_msgs::VelodyneScan::Con
   // NOTE: The header timestamp type in PCL pointclouds is narrower than in
   //       PointCloud2 msgs. We therefore copy this field directly from the
   //       losing timestamp accuracy.
-  pointcloud_corrected_msg.header = scan_msg->header;
+  pointcloud_corrected_msg.header          = scan_msg->header;
+  pointcloud_corrected_msg.header.stamp    = ros::Time().fromNSec(t_start);
   pointcloud_corrected_msg.header.frame_id = lidar_frame_id_;
 
   // publish the accumulated cloud message
