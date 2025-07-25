1ed19b28b770acebcf859f5447f363a4d3a36429
Yifu Tao <ethantao96@gmail.com>
2025-07-25 18:28:40 +0100

    update velodyne img converter

--- lidar_undistortion_ros/src/velodyne_image_converter_node.cpp
+++ lidar_undistortion_ros/src/velodyne_image_converter_node.cpp
@@ -14,35 +14,36 @@ public:
   using VelodyneCloud = VelodyneImageConverter::VelodyneCloud;
 public:
   VelodyneImageConverterNode() = delete;
-  VelodyneImageConverterNode(ros::NodeHandle& nh) : nh_(nh), img_transp_(nh_), img_cvt_(1824,16), scan_cvt_(nh_)
+  VelodyneImageConverterNode(rclcpp::Node& nh) : nh_(nh), img_cvt_(1824,16), scan_cvt_(nh_)
   {
-    cloud_sub_ = nh_.subscribe("/velodyne_packets",10, &VelodyneImageConverterNode::velodyneCloudCallback, this);
-    img_pub_ = img_transp_.advertise("velodyne_image", 1);
+    cloud_sub_ = nh_.create_subscription<velodyne_msgs::msg::VelodyneScan>(
+        "velodyne_packets", 10, std::bind(&VelodyneImageConverterNode::velodyneCloudCallback, this, std::placeholders::_1));
+    img_pub_ = nh_.create_publisher<sensor_msgs::msg::Image>("velodyne_image", 1);
     range_in_.create(128, 1824, CV_64FC1);
     azimuth_in_.create(128, 1824, CV_64FC1);
     altitude_in_.create(128, 1824, CV_64FC1);
     intensity_in_.create(128, 1824, CV_64FC1);
     reflectivity_in_.create(128, 1824, CV_64FC1);
-    ros::spin();
+    // rclcpp::spin(nh);
   }
 
-  void velodyneCloudCallback(const velodyne_msgs::msg::VelodyneScanConstPtr& msg)
+  void velodyneCloudCallback(const velodyne_msgs::msg::VelodyneScan::ConstPtr& msg)
   {
     // TODO some conversion from msg to cloud_in_
     scan_cvt_.scanToPointCloud(*msg, cloud_in_);
     img_cvt_.convert(cloud_in_, range_in_, altitude_in_, azimuth_in_, intensity_in_, reflectivity_in_);
     cv::Mat range_mono(range_in_.rows, range_in_.cols, CV_8UC1);
     img_cvt_.floatImageToMono(range_in_, range_mono);
-    sensor_msgs::ImagePtr img_msg = cv_bridge::CvImage(msg->header, "mono8", range_mono).toImageMsg();
-    img_pub_.publish(img_msg);
+    sensor_msgs::msg::Image::SharedPtr img_msg = cv_bridge::CvImage(msg->header, "mono8", range_mono).toImageMsg();
+    img_pub_->publish(img_msg);
   }
 private:
-  ros::NodeHandle& nh_;
-  image_transport::ImageTransport img_transp_;
-  image_transport::Publisher img_pub_;
+  rclcpp::Node& nh_;
+  // image_transport::ImageTransport img_transp_;
+  rclcpp::Publisher<sensor_msgs::msg::Image>::SharedPtr img_pub_;
   VelodyneImageConverter img_cvt_;
   VelodyneScanConverter<PointOuster> scan_cvt_;
-  ros::Subscriber cloud_sub_;
+  rclcpp::Subscription<velodyne_msgs::msg::VelodyneScan>::SharedPtr cloud_sub_;
   VelodyneCloud cloud_in_;
   cv::Mat range_in_;
   cv::Mat azimuth_in_;
@@ -53,8 +54,10 @@ private:
 };
 
 int main(int argc, char** argv){
-  ros::init(argc, argv, "velodyne_image_converter");
-  ros::NodeHandle nh;
-  VelodyneImageConverterNode node(nh);
+
+  rclcpp::init(argc, argv);
+  auto nh = rclcpp::Node::make_shared("velodyne_image_converter");
+  VelodyneImageConverterNode lidar_undistorter(*nh);
+  rclcpp::spin(nh);
   return 0;
 }
